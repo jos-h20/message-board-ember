3cac521486874a586048abb73fb5dc18df759e85
Josh Overly <joshoverly@yahoo.com>
2016-04-08 09:17:39 -0700

    add favorite questions service

--- app/components/question-details.js
+++ app/components/question-details.js
@@ -2,6 +2,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteQuestions: Ember.inject.service(),
+  sortBy: ['date_added:desc'],
+  sortedAnswers: Ember.computed.sort('question.answers', 'sortBy'),
   actions: {
     delete(question) {
       if (confirm('Are you sure you want to delete this question?')) {
@@ -13,6 +16,9 @@ export default Ember.Component.extend({
     },
     edit(question, params) {
       this.sendAction('edit', question, params);
+    },
+    addToFavorites(question) {
+      this.get('favoriteQuestions').add(question);
     }
   }
 });
